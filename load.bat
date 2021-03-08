ed5f51202bb0ce888be7191d7c471970e1cce1b8
Pedro Heyerdahl de Sá <61753986+pedroheyerdahl@users.noreply.github.com>
2021-03-08 16:01:47 -0300

    Update load.bat

--- load.bat
+++ load.bat
@@ -1,4 +1,4 @@
 
-for /R %f in (*.csv) do ogr2ogr -f "PostgreSQL" PG:"host=localhost port=5432 user=postgres password=mariana01 dbname=idaf schemas=sicar_07" "%f" -lco FID=id -oo AUTODETECT_TYPE=YES
-for /R %f in (*.shp) do ogr2ogr -f "PostgreSQL" PG:"host=localhost port=5432 user=postgres password=mariana01 dbname=idaf schemas=sicar_07" "%f" -nlt PROMOTE_TO_MULTI -lco GEOMETRY_NAME=geom -lco precision=NO -lco FID=id
+for /R %f in (*.csv) do ogr2ogr -f "PostgreSQL" PG:"host=localhost port=5432 user=postgres password=****** dbname=idaf schemas=sicar_07" "%f" -lco FID=id -oo AUTODETECT_TYPE=YES
+for /R %f in (*.shp) do ogr2ogr -f "PostgreSQL" PG:"host=localhost port=5432 user=postgres password=****** dbname=idaf schemas=sicar_07" "%f" -nlt PROMOTE_TO_MULTI -lco GEOMETRY_NAME=geom -lco precision=NO -lco FID=id
 
